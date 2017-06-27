bdd5cb698478deb4a24b9b7c3fe6a166a90652e2
pthim <pthim@students.kennesaw.edu>
2017-06-27 14:26:32 -0400

    Lab 7 completed.

---
+++ CS3304/lab7/main.cpp
@@ -0,0 +1,65 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 27, 2017
+ *      Author: pthim
+ */
+
+#include "deque.h"
+#include <iostream>
+#include <cassert>
+
+int main() {
+	deque<int> di;
+	deque<int> di_empty;
+	int i;
+	int a[] = {8,7,6,21,28,35,42,49,56,63};
+	int b[] = {0,1,4,9,16,25,36,49,64,81};
+	std::cout << "Created a blank deque and performing a few basic tasks: ";
+	di.push_front(7);
+	di.push_back(6);
+	di.push_front(8);
+	for (i = 3; i < di.CAPACITY; i++)
+		di.push_back(i * 7);
+	assert(di.full() && di.back() == a[9] && di.front() == a[0]);
+	std::cout << "PASSED." << std::endl;
+
+	std::cout << "Will now create a new deque using default copy constructor and passing original deque as parameter: ";
+	deque<int> di2(di);
+	assert(di2.full() && di2.back() == a[9] && di2.front() == a[0]);
+	std::cout << "PASSED." << std::endl;
+	std::cout << "Updating the new deque using pop_back: ";
+	di2.pop_back();
+	assert(!di2.full() && di2.back() == a[8]);
+	std::cout << "PASSED." << std::endl;
+	std::cout << "Verifying original is unchanged: ";
+	assert(di.full() && di.back() == a[9]);
+	std::cout << "PASSED." << std::endl;
+
+	std::cout << "Will now use default assignment operator to assign empty deque to original deque: ";
+	di = di_empty;
+	assert(di.empty());
+	std::cout << "PASSED." << std::endl;
+	std::cout << "Will now fill deque using push_back: ";
+	for (i = 0; i < di.CAPACITY; i++)
+		di.push_back(i * i);
+	assert(di.full());
+	assert(di.front() == b[0] && di.back() == b[9]);
+	std::cout << "PASSED." << std::endl;
+	std::cout << "Removing two items using pop_front: ";
+	di.pop_front();
+	di.pop_front();
+	assert(!di.full() && di.front() == b[2]);
+	std::cout << "PASSED." << std::endl;
+
+	std::cout << "Creating a deque of doubles to verify template: ";
+	deque<double> dd;
+	assert(dd.empty());
+	std::cout << "PASSED." << std::endl;
+	std::cout << "Will now fill deque using push_front: ";
+	for (i = 0; i < dd.CAPACITY; i++)
+		dd.push_back(i / 13);
+	assert(dd.full());
+	std::cout << "PASSED." << std::endl;
+	return 0;
+}
