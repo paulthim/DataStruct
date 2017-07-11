b4d3ef69fc68f36dcee3325d1855eadf41dceca5
paul <paul@10.100.57.175>
2017-07-11 10:03:36 -0400

    Assignment 6 submitted

--- CS3304/assignment6/deque.h
+++ CS3304/assignment6/deque.h
@@ -12,63 +12,63 @@ class deque
 {
 public:
     typedef std::size_t size_type;
-      
+
     //postcondition: empty deque has been created
     deque();
-    
+
     // postcondition: all resouroces allocated to the deque
     //                have been deallocated
     ~deque();
-    
+
     // postcondition: newly created deque is a copy of dq
     deque(const deque<T>& dq);
-    
-    // postcondition: current deque is a copy of dq 
+
+    // postcondition: current deque is a copy of dq
     deque<T>& operator = (const deque<T>& dq);
-    
-    
+
+
     //precondition: deque is not empty
     // postcondition: reference to element at front of deque
     //                            has been returned
     T& front();
-    
+
     // precondition: deque is not empty
     // postcondition: copy of element at front of deque
     //                            has been returned
     T front() const;
-    
+
     // precondition: deque is not empty
     // postcondition: reference to element at front of deque
     //                            has been returned
     T& back();
-    
+
     // precondition: deque is not empty
     // postcondition: copy of element at back of deque
     //                            has been returned
     T back() const;
-    
+
     // postcondition: entry has been inserted at the front
     //                            of the deque
     void push_front (const T& entry);
-    
+
     // postcondition: entry has been inserted at the back
     //                            of the deque
     void push_back (const T& entry);
-    
+
     // precondition: deque is not empty
     // postcondition: element at front of deque has been removed
     void pop_front();
-    
+
     // precondition: deque is not empty
     // postcondition: element at back of deque has been removed
     void pop_back();
-    
+
     // postcondition: number of elements in deque has been returned
     size_type size() const;
-    
+
     // postcondition: whether deque is empty has been returned
     bool empty() const;
-    
+
     // postcondition: returned whether 2 deques are equal - equal is defined
     //                            as the deques have the same number of elements &
     //                            corresponding elements are equal
